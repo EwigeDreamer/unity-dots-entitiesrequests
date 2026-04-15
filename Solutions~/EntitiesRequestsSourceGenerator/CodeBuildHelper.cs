d02cd68739d605284daeb2178538b9cf85477517
Pavel Sborshchikov <sborshikow@gmail.com>
2026-04-15 08:23:40 +0300

    source generator logic

---
+++ Solutions~/EntitiesRequestsSourceGenerator/CodeBuildHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace EntitiesRequestsSourceGenerator
+{
+    public static class CodeBuildHelper
+    {
+        private static readonly string[] PossibleAttributeNames = new[]
+        {
+            "ED.DOTS.EntitiesRequests.RegisterRequestAttribute",
+            "ED.DOTS.EntitiesRequests.RegisterRequest",
+            "RegisterRequestAttribute",
+            "RegisterRequest",
+        };
+
+        public static bool IsRegistrationAttribute(AttributeData attribute)
+        {
+            if (attribute?.AttributeClass != null)
+                return PossibleAttributeNames.Any(attribute.AttributeClass.Name.Equals);
+            return false;
+        }
+    }
+}
